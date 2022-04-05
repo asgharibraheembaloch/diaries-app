a2e21edbdb8522621e1ddb4b4a24f34c44bfb2f8
Asghar Ibraheem Baloch <asgharibraheembaloch@gmail.com>
2022-04-05 14:59:53 +0500

    initial day1 code from tutorial

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { setupServer } from './services/mirage/server';
+if (process.env.NODE_ENV === 'development') {
+  setupServer();
+}
 
 ReactDOM.render(
   <React.StrictMode>
